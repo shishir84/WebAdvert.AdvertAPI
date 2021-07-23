1e2f7e35d1378355793723383e984af28f6377de
shishir <shishirroy1982@gmail.com>
2021-07-23 15:17:14 +0530

    advert api

---
+++ AdvertApi/Controllers/AdvertController.cs
@@ -0,0 +1,91 @@
+﻿using AdvertApi.Models;
+using AdvertApi.Services;
+using AutoMapper.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("adverts/v1")]
+    [Produces("application/json")]
+    public class AdvertController : Controller
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+        public IConfiguration Configuration { get; }
+
+        public AdvertController(IAdvertStorageService advertStorageService, IConfiguration configuration)
+        {
+            _advertStorageService = advertStorageService;
+            Configuration = configuration;
+        }
+
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(201, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            string recordId;
+            try
+            {
+                recordId = await _advertStorageService.AddAsync(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+
+            return StatusCode(201, new CreateAdvertResponse { Id = recordId });
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.ConfirmAsync(model);
+                //await RaiseAdvertConfirmedMessage(model);
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception exception)
+            {
+                return StatusCode(500, exception.Message);
+            }
+
+            return new OkResult();
+        }
+
+        //private async Task RaiseAdvertConfirmedMessage(ConfirmAdvertModel model)
+        //{
+        //    var topicArn = Configuration.GetValue<string>("TopicArn");
+        //    var dbModel = await _advertStorageService.GetByIdAsync(model.Id);
+
+        //    using (var client = new AmazonSimpleNotificationServiceClient())
+        //    {
+        //        var message = new AdvertConfirmedMessage
+        //        {
+        //            Id = model.Id,
+        //            Title = dbModel.Title
+        //        };
+
+        //        var messageJson = JsonConvert.SerializeObject(message);
+        //        await client.PublishAsync(topicArn, messageJson);
+        //    }
+        //}
+    }
+}
---
+++ AdvertApi/Services/DynamoDBAdvertStorage.cs
@@ -0,0 +1,79 @@
+﻿using AdvertApi.Models;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdvertApi.Services
+{
+    public class DynamoDBAdvertStorage : IAdvertStorageService
+    {
+        private readonly IMapper _mapper;
+
+        public DynamoDBAdvertStorage(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<string> AddAsync(AdvertModel model)
+        {
+            var dbModel = _mapper.Map<AdvertDbModel>(model);
+
+            dbModel.Id = Guid.NewGuid().ToString();
+            dbModel.CreationDateTime = DateTime.UtcNow;
+            dbModel.Status = AdvertStatus.Pending;
+
+            using (var client = new AmazonDynamoDBClient())
+            {
+                var table = await client.DescribeTableAsync("Adverts");
+
+                using (var context = new DynamoDBContext(client))
+                {
+                    await context.SaveAsync(dbModel);
+                }
+            }
+
+            return dbModel.Id;
+        }
+
+        public Task<bool> CheckHealthAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ConfirmAsync(ConfirmAdvertModel model)
+        {
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var context = new DynamoDBContext(client))
+                {
+                    var record = await context.LoadAsync<AdvertDbModel>(model.Id);
+                    if (record == null) throw new KeyNotFoundException($"A record with ID={model.Id} was not found.");
+                    if (model.Status == AdvertStatus.Active)
+                    {
+                        record.FilePath = model.FilePath;
+                        record.Status = AdvertStatus.Active;
+                        await context.SaveAsync(record);
+                    }
+                    else
+                    {
+                        await context.DeleteAsync(record);
+                    }
+                }
+            }
+        }
+
+        public Task<List<AdvertModel>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AdvertModel> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
